7ecc8ab5d2c60951aa54a7cf2319143eae890f02
SharksevenRo <937257166@qq.com>
2016-04-08 21:08:37 +0800

    项目建立

---
+++ XVDZ-web/src/main/java/com/xiaov/weixin/config/ConfigDao.java
@@ -0,0 +1,16 @@
+package com.xiaov.weixin.config;
+
+import java.util.ResourceBundle;
+
+public class ConfigDao {
+	public ConfigInfo GetConfig() {
+
+		ConfigInfo Config = new ConfigInfo();
+		ResourceBundle res = ResourceBundle.getBundle("config");
+		Config.setWeChatAppID(res.getString("WeChatAppID"));
+		Config.setWeChatAppSecret(res.getString("WeChatAppSecret"));
+		Config.setWeChatToken(res.getString("WeChatToken"));
+		Config.setWeChatAESKey(res.getString("WeChatAESKey"));
+		return Config;
+	}
+}
---
+++ XVDZ-web/src/main/java/com/xiaov/weixin/controller/WechatController.java
@@ -0,0 +1,94 @@
+package com.xiaov.weixin.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.xiaov.weixin.config.ConfigDao;
+import com.xiaov.weixin.config.ConfigInfo;
+
+import me.chanjar.weixin.common.util.StringUtils;
+import me.chanjar.weixin.mp.api.WxMpInMemoryConfigStorage;
+import me.chanjar.weixin.mp.api.WxMpMessageRouter;
+import me.chanjar.weixin.mp.api.WxMpService;
+import me.chanjar.weixin.mp.api.WxMpServiceImpl;
+import me.chanjar.weixin.mp.bean.WxMpXmlMessage;
+import me.chanjar.weixin.mp.bean.WxMpXmlOutMessage;
+import me.chanjar.weixin.mp.bean.WxMpXmlOutTextMessage;
+
+@Controller
+public class WechatController {
+
+	protected WxMpInMemoryConfigStorage wxMpConfigStorage;
+	protected WxMpService wxMpService;
+	protected WxMpMessageRouter wxMpMessageRouter;
+	public void init(){ 
+		ConfigDao configDao = new ConfigDao();
+		ConfigInfo configInfo = configDao.GetConfig();
+		wxMpConfigStorage = new WxMpInMemoryConfigStorage();
+		wxMpConfigStorage.setAppId(configInfo.getWeChatAppID()); // 设置微信公众号的appid
+		wxMpConfigStorage.setSecret(configInfo.getWeChatAppSecret()); // 设置微信公众号的app
+																		// corpSecret 
+		wxMpConfigStorage.setToken(configInfo.getWeChatToken()); // 设置微信公众号的token
+		wxMpConfigStorage.setAesKey(configInfo.getWeChatAESKey()); // 设置微信公众号的EncodingAESKey
+ 
+		wxMpService = new WxMpServiceImpl();
+		wxMpService.setWxMpConfigStorage(wxMpConfigStorage);
+	} 
+	@RequestMapping("/xvdz/wechat")
+	public void XVDZWechat(HttpServletRequest request,HttpServletResponse response){
+		try { 
+			response.setContentType("text/html;charset=utf-8");
+			response.setStatus(HttpServletResponse.SC_OK);
+ 
+			String signature = request.getParameter("signature");
+			String nonce = request.getParameter("nonce");
+			String timestamp = request.getParameter("timestamp");
+ 
+			if (!wxMpService.checkSignature(timestamp, nonce, signature)) {
+				// 消息签名不正确，说明不是公众平台发过来的消息 
+				response.getWriter().println("非法请求");
+				return; 
+			} 
+ 
+			String echostr = request.getParameter("echostr");
+			if (StringUtils.isNotBlank(echostr)) {
+				// 说明是一个仅仅用来验证的请求，回显echostr 
+				response.getWriter().println(echostr);
+				return; 
+			} 
+ 
+			String encryptType = StringUtils.isBlank(request
+					.getParameter("encrypt_type")) ? "raw" : request
+					.getParameter("encrypt_type"); 
+ 
+			if ("raw".equals(encryptType)) {
+				// 明文传输的消息 
+				WxMpXmlMessage inMessage = WxMpXmlMessage.fromXml(request
+						.getInputStream()); 
+				WxMpXmlOutTextMessage m = WxMpXmlOutMessage.TEXT()
+						.fromUser(inMessage.getToUserName()).content("您好,欢迎关注小V定制微信平台")
+						.toUser(inMessage.getFromUserName()).build();
+				response.getWriter().write(m.toXml());
+				return; 
+			} 
+			if ("aes".equals(encryptType)) {
+				// 是aes加密的消息 
+				WxMpXmlMessage inMessage = WxMpXmlMessage.fromXml(request
+						.getInputStream()); 
+				WxMpXmlOutTextMessage m = WxMpXmlOutMessage.TEXT()
+						.fromUser(inMessage.getToUserName()).content("您好,欢迎关注小V定制微信平台")
+						.toUser(inMessage.getFromUserName()).build();
+				response.getWriter().write(m.toXml());
+				return; 
+			} 
+			response.getWriter().println("不可识别的加密类型");
+			return; 
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+ 
+	}
+}
