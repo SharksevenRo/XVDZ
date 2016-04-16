05391d0a048b968191374947df0f019cb49e51a9
SharksevenRo <937257166@qq.com>
2016-04-16 22:58:19 +0800

    妈的 删除你

--- XVDZ-web/src/test/java/com/xiaov/test/TestController.java
+++ XVDZ-web/src/test/java/com/xiaov/test/TestController.java
@@ -1,8 +1,33 @@
 package com.xiaov.test;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.xiaov.model.Test;
+import com.xiaov.orm.core.MessageBean;
+import com.xiaov.service.BaseService;
 
 @Controller
 public class TestController {
 
+	@Autowired
+	@Qualifier("baseService")
+	private BaseService<Test> baseService;
+	
+	@RequestMapping("/test/save")
+	@ResponseBody
+	public MessageBean save(Test user){
+		baseService.save(user);
+		return null;
+	}
+	
+	@RequestMapping("/test/update")
+	@ResponseBody
+	public MessageBean update(Test user){
+		baseService.save(user);
+		return null;
+	}
 }
