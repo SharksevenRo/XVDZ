57ba2ddd8392a177f8dcdeeb6d70a2f8b5d1edff
JIzhue <979660040@qq.com>
2016-04-23 18:48:25 +0800

    role页面表格

--- XVDZ-web/src/main/java/com/xiaov/controller/ManagerController.java
+++ XVDZ-web/src/main/java/com/xiaov/controller/ManagerController.java
@@ -6,10 +6,12 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.poi.ss.formula.functions.T;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.xiaov.model.Account;
 import com.xiaov.model.UserInfo;
 import com.xiaov.service.impl.UserServiceImpl;
 
@@ -18,21 +20,51 @@ public class ManagerController {
 	
 	@RequestMapping("/manager/getAllUser")
 	@ResponseBody
-	public List<UserInfo> AllUser(String username, HttpServletResponse response ){
-		System.out.println(username);
-		if(username.equals("User")) {
+	public String AllUser(String username, HttpServletResponse response ){
+//		System.out.println(username);
+		/*if(username.equals("User")) {
 			UserServiceImpl user = new UserServiceImpl();
 			List<UserInfo> users = user.loadAll(new UserInfo());
 			return users;
-		}else if(username.equals("Staff")) {
-			//获取所有的员工信息
+		}*/
+		StringBuilder sBuilder = new StringBuilder();
+		sBuilder.append("{");
+		sBuilder.append("\"act_id\":\"act_id\",");
+		sBuilder.append("\"us_id\":\"us_id\",");
+		sBuilder.append("\"act_mm\":\"act_mm\",");
+		sBuilder.append("\"act_tt_en\":\"act_tt_en\",");
+		sBuilder.append("\"act_blc\":\"act_blc\",");
+		sBuilder.append("\"act_fm\":\"act_fm\",");
+		sBuilder.append("\"act_state\":\"act_state\",");
+		sBuilder.append("\"update_time\":\"update_time\",");
+		sBuilder.append("\"remark\":\"remark\",");
+		sBuilder.append("\"add_time\":\"add_time\",");
+		sBuilder.append("\"delete_time\":\"delete_time\",");
+		sBuilder.append("\"act_id\":\"act_id\",");
+		sBuilder.append("\"act_id\":\"act_id\",");
+		sBuilder.append("}");
+		return sBuilder.toString();
+	}
+	
+	@RequestMapping("/manager/getAllStaff") 
+	@ResponseBody
+	public List<UserInfo> AllStaff(String username, HttpServletResponse response ){
+//		System.out.println(username);
+		if(username.equals("Staff")) {
+			//返回所有的职工信息
 		}
 		return null;
 	}
 	
+	//添加某个用户信息
 	@RequestMapping("/manager/saveUserInfo")
 	@ResponseBody
-	public void saveUser() {
-		System.out.println("OK");
+	public String saveUser() {
+		StringBuilder sBuilder = new StringBuilder();
+		sBuilder.append("{");
+		sBuilder.append("\"act_id\":\"act_id\",");
+		
+		sBuilder.append("}");
+		return null;
 	}
 }
