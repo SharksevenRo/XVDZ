8db659fc3a5b99ab7519526ed1daf23e3e9e9791
zouziyang <1253649236@qq.com>
2016-05-05 22:46:23 +0800

    获取当前用户收货地址

--- XVDZ-web/src/main/java/com/xiaov/controller/ReceiveAddressController.java
+++ XVDZ-web/src/main/java/com/xiaov/controller/ReceiveAddressController.java
@@ -4,16 +4,22 @@ import com.xiaov.constant.APPConstant;
 import com.xiaov.model.ReceiveAddress;
 import com.xiaov.orm.core.MessageBean;
 import com.xiaov.orm.core.Page;
+import com.xiaov.service.impl.ReceiveAddressServiceImpl;
 import com.xiaov.service.interfaces.ReceiveAddressService;
+import com.xiaov.utils.LazyObjecUtil;
 import com.xiaov.web.support.CookieUtil;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.asm.Label;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 /**
  * Created by zouziyang on 4/24/16.
@@ -22,6 +28,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class ReceiveAddressController {
     @Autowired
     private ReceiveAddressService receiveAddressService;
+
+    @Autowired
+    private ReceiveAddressServiceImpl receiveAddressServiceimpl;
 
     @RequestMapping("/admin/ReceiveAddress/saveAjax")
     @ResponseBody
@@ -91,4 +100,18 @@ public class ReceiveAddressController {
             return page;
         }
     }
+    @RequestMapping("admin/address/getUserAddress")
+    @ResponseBody
+    public List<ReceiveAddress> getReceiveAddress(String values) {
+        values ="1";
+        List<ReceiveAddress> result = receiveAddressServiceimpl.getByProperty("userInfo.id",values);
+        try{
+            result = LazyObjecUtil.LazySetNull(result,"userInfo");
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+
 }
