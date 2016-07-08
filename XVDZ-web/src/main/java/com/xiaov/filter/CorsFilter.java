e10c0d2fae203b595daa40a78f7f93ed67d21993
SharksevenRoot <937257166@qq.com>
2016-07-08 11:16:56 +0800

    tiaij

--- XVDZ-web/src/main/java/com/xiaov/filter/CorsFilter.java
+++ XVDZ-web/src/main/java/com/xiaov/filter/CorsFilter.java
@@ -1,6 +1,7 @@
 package com.xiaov.filter;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
@@ -10,10 +11,12 @@ import java.io.IOException;
 public class CorsFilter implements Filter{
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
-        response.setHeader("Access-Control-Allow-Origin", "*");
+        HttpServletRequest request = (HttpServletRequest) req;
+        String serverName = request.getServerName();
+        response.setHeader("Access-Control-Allow-Origin", "http://store.xiaovdingzhi.com");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+        response.setHeader("Access-Control-Allow-Headers", " Origin, No-Cache, X-Requested-With, If-Modified-Since, Pragma, Last-Modified, Cache-Control, Expires, Content-Type, X-E4M-With, Authorization");
         chain.doFilter(req, res);
     }
     public void init(FilterConfig filterConfig) {}
